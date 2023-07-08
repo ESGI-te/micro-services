ca590cb98fdb0bd004a6c700a5375c645474f55d
tlugat <teo.lugat@ausha.co>
2023-07-08 14:50:47 +0200

    ✨ Note crud auth

--- notes-api/src/app.module.ts
+++ notes-api/src/app.module.ts
@@ -1,13 +1,53 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GrpcReflectionModule } from 'nestjs-grpc-reflection';
-import { grpcConfig } from './grpc.config';
 import { PrismaService } from './prisma.service';
+import { NoteModule } from './note/note.module';
+import { GrpcReflectionModule } from 'nestjs-grpc-reflection';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { AuthModule } from './auth/auth.module';
+import grpcOption from './config/grpc.option';
+import { WinstonModule } from 'nest-winston';
+import winstonConfig from './config/winston.config';
 
+const envSchema = Joi.object({
+  DATABASE_URL: Joi.string().required(),
+  PORT: Joi.number().default(4002),
+  insecure: Joi.boolean().required(),
+  NOTE_CERT: Joi.string().when('insecure', {
+    is: false,
+    then: Joi.required(),
+  }),
+  NOTE_KEY: Joi.string().when('insecure', {
+    is: false,
+    then: Joi.required(),
+  }),
+  ROOT_CA: Joi.string().when('insecure', {
+    is: false,
+    then: Joi.required(),
+  }),
+  JAEGER_URL: Joi.string(),
+  AUTH_API_URL: Joi.string().required(),
+});
 @Module({
-  imports: [GrpcReflectionModule.register(grpcConfig)],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: envSchema,
+      isGlobal: true,
+    }),
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cs: ConfigService) => winstonConfig(cs),
+    }),
+    GrpcReflectionModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cs: ConfigService) => grpcOption(cs),
+    }),
+    NoteModule,
+    AuthModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
